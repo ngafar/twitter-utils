9c0c031988f97647cbebfafa6b9cf26d0142ce5a
Nawaz Gafar <gafar.nawaz@gmail.com>
2023-05-13 16:07:31 -0400

    Moved url and headers to top

--- twitter/endpoints.py
+++ twitter/endpoints.py
@@ -3,26 +3,27 @@ from dotenv import dotenv_values
 
 env = dotenv_values(".env")
 
+url = "https://api.twitter.com/2/users"
+headers = {"authorization": f'Bearer {env["TWITTER_BEARER_TOKEN"]}'}
+
 
 def get_user_id(twitter_username):
-    url = f"https://api.twitter.com/2/users/by?usernames={twitter_username}"
-
-    headers = {"authorization": f'Bearer {env["TWITTER_BEARER_TOKEN"]}'}
-
-    response = requests.request("GET", url, headers=headers)
+    endpoint = url + f"/by?usernames={twitter_username}"
+    
+    response = requests.request("GET", endpoint, headers=headers)
     user_id = response.json()["data"][0]["id"]
 
     return user_id
 
+
 def get_likes(user_id):
-    url = f"https://api.twitter.com/2/users/{user_id}/liked_tweets"
+    endpoint = url + f"/{user_id}/liked_tweets"
 
-    headers = {"authorization": f'Bearer {env["TWITTER_BEARER_TOKEN"]}'}
-
-    response = requests.request("GET", url, headers=headers)
+    response = requests.request("GET", endpoint, headers=headers)
     likes = response.json()["data"]
 
     return likes
+
 
 user_id = get_user_id("NawazGafar")
 likes = get_likes(user_id)
