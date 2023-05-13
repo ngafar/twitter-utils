9f867dd22e7d410eaba26ba8cf9c8d1fa37c42b4
Nawaz Gafar <gafar.nawaz@gmail.com>
2023-05-13 19:37:25 -0400

    Added liked_tweets params

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ from utils import export
 user_id = endpoints.get_user_id("NawazGafar")
 
 all_likes = []
+all_media = []
+all_users = []
 pagination_token = None
 
 while True:
-    likes, pagination_token = endpoints.get_likes(user_id, pagination_token)
+    likes, media, users, pagination_token = endpoints.get_likes(user_id, pagination_token)
     all_likes.extend(likes)
+    all_media.extend(media)
+    all_users.extend(users)
 
     if pagination_token is None:
         break
-
-print(len(all_likes))
 
 export.list_to_json(all_likes)
--- twitter/endpoints.py
+++ twitter/endpoints.py
@@ -18,16 +18,34 @@ def get_user_id(twitter_username):
 
 def get_likes(user_id, pagination_token=None):
     endpoint = url + f"/{user_id}/liked_tweets"
+    params_expansions = "?expansions=attachments.media_keys,author_id"
+    params_media_fields = "&media.fields=alt_text,preview_image_url,type,url"
+    params_tweet_fields = "&tweet.fields=created_at,referenced_tweets,source"
+    params_user_fields = "&user.fields=id,name,profile_image_url,username"
+
+    endpoint += (
+        params_expansions
+        + params_media_fields
+        + params_tweet_fields
+        + params_user_fields
+    )
     if pagination_token:
-        endpoint = endpoint + f"?pagination_token={pagination_token}"
+        endpoint = endpoint + f"&pagination_token={pagination_token}"
+
+    print(endpoint)
 
     response = requests.request("GET", endpoint, headers=headers)
 
+    likes = response.json().get("data", [])
+    media = response.json().get("includes", {}).get("media", [])
+    users = response.json().get("includes", {}).get("users", [])
+    next_token = response.json().get("meta", {}).get("next_token", None)
+    """
     try:
         likes = response.json()["data"]
         next_token = response.json()["meta"].get("next_token")
     except:
         likes = []
         next_token = None
-
-    return likes, next_token
+    """
+    return likes, media, users, next_token
