f809c06239969de3168c13e81f7385073fb14f6d
Nawaz Gafar <gafar.nawaz@gmail.com>
2023-05-18 22:39:06 -0400

    Created workflows dir

--- main.py
+++ main.py
@@ -1,38 +1,8 @@
-from twitter import endpoints
-from utils import export 
+import sys
+from workflows import get_likes
 
-user_id = endpoints.get_user_id("NawazGafar")
-
-all_likes = []
-all_media = []
-all_users = []
-pagination_token = None
-
-while True:
-    likes, media, users, pagination_token = endpoints.get_likes(user_id, pagination_token)
-    all_likes.extend(likes)
-    all_media.extend(media)
-    all_users.extend(users)
-
-    if pagination_token is None:
-        break
-
-for tweet in all_likes:
-    # look for matching media
-    try:
-        media_keys = tweet["attachments"]["media_keys"]
-
-        for media in all_media:
-            if media["media_key"] in media_keys:
-                tweet["media"] = media
-    except:
-        pass
+if __name__ == "__main__":
+    arg = sys.argv[1].lower()
     
-    # look for matching user
-    author_id = tweet["author_id"]
-
-    for user in all_users:
-        if user["id"] == author_id:
-            tweet["author"] = user
-            
-export.list_to_json(all_likes)
+    if arg == "get_likes":
+        get_likes.get_likes('NawazGafar')
---
+++ workflows/test.py
@@ -0,0 +1,5 @@
+def test():
+    return "Hello World"
+
+def test_with_arg(arg):
+    return arg
