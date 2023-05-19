4023b48c7e39b30185dc67e5b0caecbaf205083f
Nawaz Gafar <gafar.nawaz@gmail.com>
2023-05-18 22:41:27 -0400

    Added twitter username to env

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import sys
+from dotenv import dotenv_values
 from workflows import get_likes
+
+env = dotenv_values(".env")
+twitter_username = env["TWITTER_USERNAME"]
 
 if __name__ == "__main__":
     arg = sys.argv[1].lower()
     
     if arg == "get_likes":
-        get_likes.get_likes('NawazGafar')
+        get_likes.get_likes(twitter_username)
